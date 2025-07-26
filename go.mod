b4a3b7093162d762de831eb2351884ad4b46deff
iO <senpai9464@yandex.ru>
2025-07-26 19:26:46 +0300

    init

---
+++ go.mod
@@ -0,0 +1,17 @@
+module root
+
+go 1.23.4
+
+require (
+	github.com/btcsuite/btcd v0.24.2
+	github.com/btcsuite/btcd/btcec/v2 v2.1.3
+	github.com/btcsuite/btcd/btcutil v1.1.6
+	github.com/tyler-smith/go-bip39 v1.1.0
+)
+
+require (
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/sys v0.0.0-20200814200057-3d37ad5750ed // indirect
+)
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/tyler-smith/go-bip39"
+)
+
+type Wallet struct {
+	Mnemonic string
+	Address  string
+}
+
+func NewWallet() (*Wallet, error) {
+	entropy := make([]byte, 32)
+	if _, err := rand.Read(entropy); err != nil {
+		return nil, err
+	}
+
+	mnemonic, err := bip39.NewMnemonic(entropy)
+	if err != nil {
+		return nil, err
+	}
+
+	seed := bip39.NewSeed(mnemonic, "")
+	masterKey, _ := btcec.PrivKeyFromBytes(seed[:32])
+
+	wif, err := btcutil.NewWIF(masterKey, &chaincfg.MainNetParams, true)
+	if err != nil {
+		return nil, err
+	}
+
+	address, err := btcutil.NewAddressPubKey(wif.PrivKey.PubKey().SerializeCompressed(), &chaincfg.MainNetParams)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Wallet{
+		Mnemonic: mnemonic,
+		Address:  address.EncodeAddress(),
+	}, nil
+}
+
+func main() {
+	wallet, err := NewWallet()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Mnemonic:", wallet.Mnemonic)
+	fmt.Println("Address:", wallet.Address)
+}
